df11a43d95c2313251f79b44618d5fe1e2719705
Vikranth Apps <imakingapps@gmail.com>
2020-09-16 15:35:33 +0530

    first update v  commit

---
+++ ProjectFramework.framework/Headers/ProjectFramework.h
@@ -0,0 +1,19 @@
+//
+//  ProjectFramework.h
+//  ProjectFramework
+//
+//  Created by Vikranth Naidu on 16/09/20.
+//  Copyright © 2020 Vikranth Naidu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ProjectFramework.
+FOUNDATION_EXPORT double ProjectFrameworkVersionNumber;
+
+//! Project version string for ProjectFramework.
+FOUNDATION_EXPORT const unsigned char ProjectFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProjectFramework/PublicHeader.h>
+
+
